c645650e710c48dab56ff7895fae1b457e44af3e
eda3 <gerorin1010@gmail.com>
2019-09-19 06:58:28 +0900

    first commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,33 @@
+function touch {
+    New-Item -type file $args
+}
+
+function gs {
+    git status $args
+}
+function gl {
+    git log --oneline --decorate --graph --branches --tags --remotes --name-status -5
+
+}
+function gf {
+    git diff $args
+}
+function gfs {
+    git diff --staged
+}
+
+function gaa {
+    git add -A
+}
+function gcom {
+    git commit -m $args
+}
+
+function ll {
+   Get-ChildItem $args
+}
+
+
+function open {
+    Invoke-Item $args
+}
